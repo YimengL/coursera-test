21af43ea58f076ef77c8425f362ac3e547d44416
Yimeng Li <lym1108csu@gmail.com>
2017-07-16 22:41:33 -0400

    modify

--- c5_module5_sol/src/common/common.module.js
+++ c5_module5_sol/src/common/common.module.js
@@ -2,7 +2,7 @@
 "use strict";
 
 angular.module('common', [])
-.constant('ApiPath', 'https://ychaikin-course5.herokuapp.com')
+.constant('ApiPath', 'https://dkasti-coursera-spa.herokuapp.com')
 .config(config);
 
 config.$inject = ['$httpProvider'];
